35b2b544fc2f33a7d531ad64371406e0c5c05ff3
raimu <ortwang@googlemail.com>
2016-12-24 13:24:27 +0100

    Copy also the 3rd party files to the dist folder
    So that the needed 3rd party components are there for run the
    application. Additionally add the dist folder into the gitignore,
    because the repository don't need it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+var gulpCopy = require('gulp-copy');
 
 gulp.task('typescript', function () {
   var tsProject = ts.createProject('tsconfig.json');
@@ -8,4 +9,28 @@ gulp.task('typescript', function () {
     .pipe(gulp.dest('./dist/app'));
 });
 
-gulp.task('default', ['typescript']);
+gulp.task('copy-app', function() {
+  return gulp
+    .src('app/**/*.html', {base: './app'})
+    .pipe(gulp.dest('./dist/app'))
+});
+
+gulp.task('copy-3rd-party', function() {
+  return gulp
+    .src([
+      'node_modules/material-design-lite/material.min.css',
+      'node_modules/material-design-lite/material.min.js',
+      'node_modules/material-design-icons/iconfont/material-icons.css',
+      'node_modules/material-design-icons/iconfont/MaterialIcons-Regular.woff2',
+      'node_modules/material-design-icons/iconfont/MaterialIcons-Regular.woff',
+      'node_modules/core-js/client/shim.min.js',
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/reflect-metadata/Reflect.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/@angular/**/*js',
+      'node_modules/rxjs/**/*.js'
+      ], {base: './node_modules'})
+    .pipe(gulp.dest('./dist/node_modules'))
+});
+
+gulp.task('default', ['typescript', 'copy-3rd-party', 'copy-app']);
