297406a3bdb22e128d613e7ab45ef155172b8e91
raimu <ortwang@googlemail.com>
2017-01-10 20:55:38 +0100

    Add the angular material2 library
    For some widgets the mdl library is not enough, therefore try the
    library special for angular 2, side by side. So that i can use the
    best solution from both sides.
    (As long as the angular-material is still beta)

--- app/groceries/groceries.module.ts
+++ app/groceries/groceries.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 
 import { GroceriesRoutingModule } from './groceries-routing.module';
 import { GroceriesComponent } from './groceries.component';
@@ -13,6 +14,7 @@ import { ItemService } from './item.service';
   imports: [
     BrowserModule,
     FormsModule,
+    MaterialModule.forRoot(),
     GroceriesRoutingModule
   ],
   declarations: [
