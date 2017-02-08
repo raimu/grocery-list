f0ceedf079d246907e1b5a136bed2da8c338dfba
raimu <ortwang@googlemail.com>
2017-02-08 19:41:31 +0100

    Fix the existing unittest for the main app component
    So that the test, which was copied from the angular-quickstart works
    also for this app. No special logic, just initiate the component and
    check the headline.

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -1,8 +1,12 @@
-import { AppComponent } from './app.component';
-
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+import { ToolbarService }  from './toolbar.service';
+import { CoreModule }      from './core/core.module';
+
 
 describe('AppComponent', function () {
   let de: DebugElement;
@@ -11,7 +15,9 @@ describe('AppComponent', function () {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      declarations: [ AppComponent ],
+      imports: [ CoreModule, RouterTestingModule ],
+      providers: [ ToolbarService ]
     })
     .compileComponents();
   }));
@@ -19,15 +25,14 @@ describe('AppComponent', function () {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
+    de = fixture.debugElement.query(By.css('div'));
   });
 
   it('should create component', () => expect(comp).toBeDefined() );
 
-  it('should have expected <h1> text', () => {
-    fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
+  it('should have expected headline text', () => {
+    const content = de.nativeElement;
+    expect(content.innerText).toMatch(/Grocery list/i,
+      'headline should contains the app name "Grocery list"');
   });
 });
