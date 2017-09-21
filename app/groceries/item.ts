1d87ea6efbd79067633dbd5a8b0512c0cd83e70a
raimu <ortwang@googlemail.com>
2017-09-21 20:43:28 +0200

    Restart the app development
    In the meantime angular gets a lot more features. For example the update
    from 2.4 to 4.2.4, and the angular-cli instead of self written grunt tasks.
    The main-reason for this grocery app is to lern technices, and to play around.
    Therefore make an clean cut and rewrite this app.
    
    - Delete the old angular app.
    - Remain an few files, like the license or cordova stuff, because they are
      still the same.
    - Generate with the cli an new base structure (currently empty).
    -> It's now an empty app withouth any functionality.
    
    The next steps would be to reimplement all features, so that's hopefully it
    works at the end like before.

--- app/groceries/item.ts
+++
@@ -1,8 +0,0 @@
-export class Item {
-  _id: string;
-  _rev: string;
-  amount?: number;
-  unit?: string;
-  name: string;
-  done?: boolean;
-}
--- app/groceries/mock-items.ts
+++
@@ -1,8 +0,0 @@
-import { Item } from './item';
-
-export const ITEMS: Item[] = [
-  { _id: '1', _rev: '1', amount: 400, unit: 'g', name: 'flour' },
-  { _id: '2', _rev: '1', amount: 1, unit: 'kg', name: 'sugar' },
-  { _id: '3', _rev: '1', amount: 10, name: 'eggs' },
-  { _id: '4', _rev: '1', name: 'soda' },
-];
