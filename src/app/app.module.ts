b7add395b694bac12bec0da8b8f30f2542698918
raimu <ortwang@googlemail.com>
2017-12-18 20:46:09 +0100

    Add service worker
    It's the default service-worker of angular – caches the app, so that
    all runs faster.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppMaterialModule } from './app-material.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +11,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { ItemListComponent } from './item-list/item-list.component';
 import { AddItemComponent } from './add-item/add-item.component';
 import { ItemService } from './item.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,7 @@ import { ItemService } from './item.service';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     AppMaterialModule,
     AppRoutingModule
   ],
